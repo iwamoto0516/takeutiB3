09608941a66e825dca965ee6da57d52ab86d286e
359 <280359z@ugs.kochi-tech.ac.jp>
2026-06-24 10:53:15 +0900

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Manabu!");
         System.out.println("Hello rukito");
+        System.out.println("Hello world!");
     }
 }
