95133ed89f12780d6caec10ff62ef2e8039deacd
ogasawarayuzuki <ogasan.yuzuki@gmail.com>
2026-06-24 10:59:00 +0900

    Hello Mr Takeuchiを追記

--- Hello.java
+++ Hello.java
@@ -4,5 +4,6 @@ public class Hello {
         System.out.println("Hello rukito");
         System.out.println("Hello world!");
         System.out.println("Thank you!");
+        System.out.println("Hello Mr Takeuchi!!");
     }
 }
