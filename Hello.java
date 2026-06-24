cbbd2c78920df6803f131c69dc4382d41bcaed28
MasuyaManabu <mamananabubu0620123456@gmail.com>
2026-06-24 10:54:11 +0900

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public class Hello {
         System.out.println("Hello Manabu!");
         System.out.println("Hello rukito");
         System.out.println("Hello world!");
+        System.out.println("Thank you!");
     }
 }
