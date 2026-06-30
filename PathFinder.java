75b4d7e058bae109daf78db7c72c54a8d1509b58
280365d-dotcom <280365d@ugs.kochi-tech.ac.jp>
2026-07-01 08:31:18 +0900

    経路があるのかを探索するPathFinder.javaのテンプレートを作成した

---
+++ PathFinder.java
@@ -0,0 +1,28 @@
+public class PathFinder {
+
+  public static final int EMPTY = 0;
+  public static final int WALL = 1;
+  public static final int BLACK = 2;
+  public static final int WHITE = 3;
+
+  public boolean hasPath(Board board, int player) {
+
+    BoardMap boardMap = new BoardMap();
+    // 17×17の盤面を作成
+    int[][] map = boardMap.createMap(board);
+
+    // 探索開始位置
+    int startRow;
+    int startCol;
+
+    if (player == Board.BLACK) {
+        startRow = board.blackRow * 2;
+        startCol = board.blackCol * 2;
+    } else {
+        startRow = board.whiteRow * 2;
+        startCol = board.whiteCol * 2;
+    }
+
+    return true; // またはfalse
+  }
+}
