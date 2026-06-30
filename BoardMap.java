83b00d1b5af4b9efcecf2a03b4bc1717ace5e166
280365d-dotcom <280365d@ugs.kochi-tech.ac.jp>
2026-07-01 08:28:31 +0900

    プレイヤーと壁の情報を持った17x17の配列を作るBoardMap.javaを作成した

---
+++ BoardMap.java
@@ -0,0 +1,42 @@
+public class BoardMap {
+  public static final int EMPTY = 0;
+  public static final int WALL = 1;
+  public static final int BLACK = 2;
+  public static final int WHITE = 3;
+
+  public int[][] createMap(Board board) {
+    int[][] map = new int[17][17];
+    // 横壁
+    for (int row = 0; row < 8; row++) {
+        for (int col = 0; col < 8; col++) {
+
+            if (board.horizontalCenter[row][col]) {
+                map[row * 2 + 1][col * 2] = WALL;
+                map[row * 2 + 1][col * 2 + 1] = WALL;
+                map[row * 2 + 1][col * 2 + 2] = WALL;
+            }
+        }
+    }
+
+    // 縦壁
+    for (int row = 0; row < 8; row++) {
+        for (int col = 0; col < 8; col++) {
+
+            if (board.verticalCenter[row][col]) {
+                map[row * 2][col * 2 + 1] = WALL;
+                map[row * 2 + 1][col * 2 + 1] = WALL;
+                map[row * 2 + 2][col * 2 + 1] = WALL;
+            }
+        }
+    }
+
+    // 黒駒
+    map[board.blackRow * 2][board.blackCol * 2] = BLACK;
+
+    // 白駒
+    map[board.whiteRow * 2][board.whiteCol * 2] = WHITE;
+
+    return map;
+  }
+}
+
