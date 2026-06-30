8c8af8bbc4f6bd704ae134f7742aee0c0bb1e9b3
280365d-dotcom <280365d@ugs.kochi-tech.ac.jp>
2026-07-01 06:45:41 +0900

    壁の設置を行うWallPlace.javaを追加した

---
+++ WallPlace.java
@@ -0,0 +1,87 @@
+// 壁の設置
+public class WallMove {
+  // 横壁の設置
+  public boolean placeHorizontalWall(Board board,int row, int col) {
+
+    // 手持ちの壁があるかを確認
+    if(board.turn == Board.BLACK && board.blackWalls <= 0) {
+      return false;
+    }
+    if(board.turn == Board.WHITE && board.whiteWalls <= 0) {
+      return false;
+    }
+
+    // 設置する場所が盤面内か確認する
+    if(row < 0 || row >= 8 || col < 0 || col >= 8) {
+      return false;
+    }
+
+    // 交差する壁があるか確認
+    if(board.verticalCenter[row][col]) {
+      return false;
+    }
+
+    // 設置する場所に壁がないか確認
+    if(board.horizontalWall[row][col] || board.horizontalWall[row][col+1]) {
+      return false;
+    }
+
+    // 壁の設置
+    board.horizontalWall[row][col] = true;
+    board.horizontalWall[row][col+1] = true;
+    board.horizontalCenter[row][col] = true;
+
+    // 手持ちの壁の枚数の更新と手番交代
+    if(board.turn == Board.BLACK) {
+      board.blackWalls--;
+      board.turn = Board.WHITE;
+    } else {
+      board.whiteWalls--;
+      board.turn = Board.BLACK;
+    }
+    return true;
+  }
+
+  // 縦壁の設置
+  public boolean placeVerticalWall(Board board,int row, int col) {
+
+    // 手持ちの壁があるかを確認
+    if(board.turn == Board.BLACK && board.blackWalls <= 0) {
+      return false;
+    }
+    if(board.turn == Board.WHITE && board.whiteWalls <= 0) {
+      return false;
+    }
+
+    // 設置する場所が盤面内か確認する
+    if(row < 0 || row >= 8 || col < 0 || col >= 8) {
+      return false;
+    }
+
+    // 交差する壁があるか確認
+    if(board.horizontalCenter[row][col]) {
+      return false;
+    }
+
+    //設置する場所に壁がないか確認
+    if(board.verticalWall[row][col] || board.verticalWall[row+1][col]) {
+      return false;
+    }
+
+    // 壁の設置
+    board.verticalWall[row][col] = true;
+    board.verticalWall[row+1][col] = true;
+    board.verticalCenter[row][col] = true;
+
+    // 手持ちの壁の枚数の更新と手番交代
+    if(board.turn == Board.BLACK) {
+      board.blackWalls--;
+      board.turn = Board.WHITE;
+    } else {
+      board.whiteWalls--;
+      board.turn = Board.BLACK;
+    }
+    return true;
+  }
+
+}
